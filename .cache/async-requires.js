db38c0f1000a76002a774b338dd4bb8368620b61
andrey.tarasov <the.withered.fern@gmail.com>
2018-04-17 12:39:51 +0300

    fix

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,35 +2,26 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/aim-imac-3/projects/wFern.github.io/.cache/dev-404-page.js"),
   "component---src-templates-blog-post-js": require("gatsby-module-loader?name=component---src-templates-blog-post-js!/Users/aim-imac-3/projects/wFern.github.io/src/templates/blog-post.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/aim-imac-3/projects/wFern.github.io/src/pages/404.js"),
   "component---src-pages-blog-en-js": require("gatsby-module-loader?name=component---src-pages-blog-en-js!/Users/aim-imac-3/projects/wFern.github.io/src/pages/blog.en.js"),
   "component---src-pages-blog-ru-js": require("gatsby-module-loader?name=component---src-pages-blog-ru-js!/Users/aim-imac-3/projects/wFern.github.io/src/pages/blog.ru.js"),
-  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/aim-imac-3/projects/wFern.github.io/src/pages/index.js"),
   "component---src-pages-index-en-js": require("gatsby-module-loader?name=component---src-pages-index-en-js!/Users/aim-imac-3/projects/wFern.github.io/src/pages/index.en.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/aim-imac-3/projects/wFern.github.io/src/pages/index.js"),
   "component---src-pages-index-ru-js": require("gatsby-module-loader?name=component---src-pages-index-ru-js!/Users/aim-imac-3/projects/wFern.github.io/src/pages/index.ru.js")
 }
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/dev-404-page.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "en-hello-world.json": require("gatsby-module-loader?name=path---en-hello-world!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/en-hello-world.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "ru-hello-world.json": require("gatsby-module-loader?name=path---ru-hello-world!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/ru-hello-world.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/404.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "en-blog.json": require("gatsby-module-loader?name=path---en-blog!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/en-blog.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "ru-blog.json": require("gatsby-module-loader?name=path---ru-blog!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/ru-blog.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
-  "index.json": require("gatsby-module-loader?name=path---index!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/index.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "en.json": require("gatsby-module-loader?name=path---en!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/en.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
-  "ru.json": require("gatsby-module-loader?name=path---ru!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/ru.json")
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/index.json"),
+  "ru.json": require("gatsby-module-loader?name=path---ru!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/ru.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/aim-imac-3/projects/wFern.github.io/.cache/json/404-html.json")
 }
 
 exports.layouts = {
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -2,38 +2,29 @@
 const preferDefault = m => m && m.default || m
 
 
+exports.layouts = {
+  "layout---index": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/.cache/layouts/index.js"))
+}
+
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/.cache/dev-404-page.js")),
   "component---src-templates-blog-post-js": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/src/templates/blog-post.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/src/pages/404.js")),
   "component---src-pages-blog-en-js": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/src/pages/blog.en.js")),
   "component---src-pages-blog-ru-js": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/src/pages/blog.ru.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/src/pages/index.js")),
   "component---src-pages-index-en-js": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/src/pages/index.en.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/src/pages/index.js")),
   "component---src-pages-index-ru-js": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/src/pages/index.ru.js"))
 }
 
 exports.json = {
   "layout-index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/dev-404-page.json"),
-  "layout-index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "en-hello-world.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/en-hello-world.json"),
-  "layout-index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "ru-hello-world.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/ru-hello-world.json"),
-  "layout-index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "404.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/404.json"),
-  "layout-index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "en-blog.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/en-blog.json"),
-  "layout-index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "ru-blog.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/ru-blog.json"),
-  "layout-index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
-  "index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/index.json"),
-  "layout-index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
   "en.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/en.json"),
-  "layout-index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/layout-index.json"),
-  "ru.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/ru.json")
-}
-
-exports.layouts = {
-  "layout---index": preferDefault(require("/Users/aim-imac-3/projects/wFern.github.io/.cache/layouts/index.js"))
+  "index.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/index.json"),
+  "ru.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/ru.json"),
+  "404-html.json": require("/Users/aim-imac-3/projects/wFern.github.io/.cache/json/404-html.json")
 }
