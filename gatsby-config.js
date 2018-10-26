c61c9513f9c7aeb3ace4e646ae7816339976c3fe
Andrey <the.withered.fern@gmail.com>
2018-10-26 16:01:07 +0300

    add plugins and analitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,29 +9,62 @@ module.exports = {
     languages
   },
   plugins: [
-      `gatsby-plugin-react-helmet`,
-      'gatsby-plugin-catch-links',
-      {
-          resolve: 'gatsby-plugin-i18n',
-          options: {
-              langKeyForNull: 'any',
-              langKeyDefault: languages.defaultLangKey,
-              useLangKeyLayout: false
-          }
+    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-plugin-i18n',
+      options: {
+        langKeyForNull: 'any',
+        langKeyDefault: languages.defaultLangKey,
+        useLangKeyLayout: false
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages/blog`,
+        name: 'blog',
       },
-      {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-              path: `${__dirname}/src/pages/blog`,
-              name: 'blog',
-          },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+          plugins: []
+      }
+    },
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-128212134-1",
+        // // Puts tracking script in the head instead of the body
+        // head: false,
+        // // Setting this parameter is optional
+        // anonymize: true,
+        // // Setting this parameter is also optional
+        // respectDNT: true,
+        // // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // // Any additional create only fields (optional)
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "example.com",
       },
-      {
-          resolve: 'gatsby-transformer-remark',
-          options: {
-              plugins: []
-          }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "salad_nights",
+        short_name: "salad_nights",
+        start_url: "/",
+        background_color: "#000506",
+        theme_color: "#DCAE61",
+        display: "minimal-ui",
+        icon: "src/assets/img/salad_nights_icon.png", // This path is relative to the root of the site.
       },
-      'gatsby-plugin-sass',
+    },
+    'gatsby-plugin-offline'
   ],
 };
